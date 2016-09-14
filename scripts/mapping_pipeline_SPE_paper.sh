27029bd8b67946aad9417e182acd7c32bf4e03e5
Guy Wachsman <guywachsman@wl-10-190-104-226.wireless.duke.edu>
2016-09-14 10:39:38 -0400

    pipeline works now with a link to empty gz file in public repo; link is identical to txt file after pressing row with gz extension to the link name

--- scripts/mapping_pipeline_SPE_paper.sh
+++ scripts/mapping_pipeline_SPE_paper.sh
@@ -54,9 +54,9 @@ fi
 
 #downloading & creating knownsnps file
 knownsnps_link=`awk -v var="$my_species" 'match($1, var) {print $3}' ./scripts/data_base.txt`
-if ! [ -f ./refs/$my_species.vcf ]; then
-  curl -LOK ./refs/$my_species.vcf.gz
-  gzip -d ./refs/$my_species.vcf.gz
+if ! [ -f ./refs/$my_species.txt ]; then
+  curl -o ./refs/$my_species.txt.gz $knownsnps_link
+  gzip -d ./refs/$my_species.txt.gz
 fi
 
 #snpEff "link"
